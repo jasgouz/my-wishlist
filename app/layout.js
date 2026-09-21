60fd8ae0e6f9b0eea01c743243ab57a45e80545c
Jasurbek Ne’matov <nematov.j.sh@gmail.com>
2026-09-21 18:03:46 +0500

    feat: add wishlist layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata = { title: 'Мои хотелки', description: 'Личный список желаний' };
+export default function RootLayout({ children }) { return <html lang="ru"><body>{children}</body></html>; }
