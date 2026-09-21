88534ea78aeeb597763e07d7b357c96362d6b3b9
Jasurbek Ne’matov <nematov.j.sh@gmail.com>
2026-09-21 18:28:41 +0500

    fix: update Viltrox product photo

--- app/layout.js
+++
@@ -1,1 +0,0 @@
-import './globals.css'; export const metadata={title:'Мои хотелки',description:'Личный список желаний'}; export default function RootLayout({children}){return <html lang="ru"><body>{children}</body></html>}
